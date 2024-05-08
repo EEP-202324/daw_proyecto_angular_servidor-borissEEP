ffe26b9fe4bfebd24515e9902f1cd90fb3605061
borissEEP <balexis-ruiz1@eep-igroup.com>
2024-05-08 09:16:41 +0200

    Componente JugadorLocation creado

--- Angular/jugadores/src/app/intro/intro.component.ts
+++ Angular/jugadores/src/app/intro/intro.component.ts
@@ -1,19 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { JugadorLocationComponent } from '../jugador-location/jugador-location.component';
+import { Jugadorlocation } from '../jugadorlocation';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    JugadorLocationComponent
+  ],
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by player">
-      <button class="primary" type="button">Buscar</button>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
     </form>
   </section>
-`,
+  <section class="results">
+    <app-jugador-location></app-jugador-location>
+  </section>
+  `,
   styleUrl: './intro.component.css'
 })
 export class IntroComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  jugadorLocation: Jugadorlocation = {
+    id: 9999,
+    name: 'Messi Test',
+    city: 'Test Messi',
+    state: 'AR',
+    photo: `${this.baseUrl}/example-house.jpg`,
+
+  };
 }
---
+++ Angular/jugadores/src/app/jugador-location/jugador-location.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jugador-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      jugador-location works!
+    </p>
+  `,
+  styleUrl: './jugador-location.component.css'
+})
+export class JugadorLocationComponent {
+
+}
